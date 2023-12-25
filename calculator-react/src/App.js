63ea19c5aad5e12b82d421ad63473b3c2e07944d
Rogelio <roger.g.m@hotmail.com>
2023-12-24 18:47:22 -0600

    finish proyect calculator

--- calculator-react/src/App.js
+++ calculator-react/src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Boton  from './componentes/boton';
 import Pantalla from './componentes/pantalla';
 import BotonClear from './componentes/clear';
 import { useState } from 'react';
+import { evaluate } from 'mathjs';
 
 function App() {
 
@@ -12,6 +14,14 @@ function App() {
     const addInput = val => {
       setInput(input + val);
     };
+
+    const calcularResulatod = ()=> {
+      if(input) {
+        setInput(evaluate(input));
+      } else {
+        alert('Favor de ingresar un valor')
+      }
+    }
 
   return (
     <div className="App">
@@ -33,25 +43,27 @@ function App() {
           <Boton manejarInput={addInput}>+</Boton>
         </div>
         <div className='fila'>
-        <Boton manejarInput={addInput}>4</Boton>
-        <Boton manejarInput={addInput}>5</Boton>
-        <Boton manejarInput={addInput}>6</Boton>
-        <Boton manejarInput={addInput}>-</Boton>
+          <Boton manejarInput={addInput}>4</Boton>
+          <Boton manejarInput={addInput}>5</Boton>
+          <Boton manejarInput={addInput}>6</Boton>
+          <Boton manejarInput={addInput}>-</Boton>
         </div>
         <div className='fila'>
-        <Boton manejarInput={addInput}>7</Boton>
-        <Boton manejarInput={addInput}>8</Boton>
-        <Boton manejarInput={addInput}>9</Boton>
-        <Boton manejarInput={addInput}>*</Boton>
+          <Boton manejarInput={addInput}>7</Boton>
+          <Boton manejarInput={addInput}>8</Boton>
+          <Boton manejarInput={addInput}>9</Boton>
+          <Boton manejarInput={addInput}>*</Boton>
         </div>
         <div className='fila'>
-        <Boton manejarInput={addInput}>=</Boton>
-        <Boton manejarInput={addInput}>0</Boton>
-        <Boton manejarInput={addInput}>.</Boton>
-        <Boton manejarInput={addInput}>/</Boton>
+          <Boton manejarInput={calcularResulatod}>=</Boton>
+          <Boton manejarInput={addInput}>0</Boton>
+          <Boton manejarInput={addInput}>.</Boton>
+          <Boton manejarInput={addInput}>/</Boton>
         </div>
         <div className='fila'>
-        <BotonClear >Clear</BotonClear>
+          <BotonClear manejarClear={() => setInput('')} >
+            Clear
+          </BotonClear>
         </div>
       </div>
     </div>
--- calculator-react/src/componentes/clear.js
+++ calculator-react/src/componentes/clear.js
@@ -2,7 +2,9 @@ import React from 'react';
 import '../style/botonClear.css'
 
 const BotonClear = (props) => (
-    <div className='botonClear'>
+    <div className='botonClear'
+    onClick={props.manejarClear}
+    >
         {props.children}
     </div>
 );
