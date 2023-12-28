8b28792bc8dfde8bb0575d18d70f5ff860f8b3f7
Rogelio <roger.g.m@hotmail.com>
2023-12-27 20:42:13 -0600

    Update

---
+++ lista-de-tareas/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='contImg'>
+        <img 
+        src={logo} 
+        className='img-logo' 
+        alt />
+
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
